56e3500c4e44f6b9446478ae768aed6f1c84f924
Samer Zoorob <zsamer305@gmail.com>
2026-02-13 20:57:04 +0100

    Fix Render port

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def update(id):
         return render_template('update.html', task=task)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("port". 5000))
+    port = int(os.environ.get("port", 5000))
     app.run(host="0.0.0.0", port=port)
